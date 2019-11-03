d9eaa8311067b5e98e04beb40002579c81d2b060
JEFFRYDANIEL <jgalansenmache@gmail.com>
2019-11-03 17:18:35 -0500

    SE AGREGO LAS 1ERA CALCULADORA

---
+++ CALCULADORA 1.py
@@ -0,0 +1,16 @@
+#CALCULAR EL AREA DEL TRIANGULO
+area=0.0
+base=0.0
+altura=0.0
+
+#ASIGNACION DE VALORES
+base=25.0
+altura=18.0
+
+#CALCULO
+area=(base*altura)/2
+
+#MOSTRAR CALCULO
+print("base=",base)
+print("altura=",altura)
+print("area=",area)
